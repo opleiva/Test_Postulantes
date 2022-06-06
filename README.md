742dee8d3eaa6e08d2e55b358bf8e9088cb43bf0
opleiva <uas_ingeco@yahoo.com>
2022-06-05 21:51:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-pndk1s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-pndk1s)
---
+++ src/app/pag1/pag1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pag1',
+  templateUrl: './pag1.component.html',
+  styleUrls: ['./pag1.component.css']
+})
+export class Pag1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
